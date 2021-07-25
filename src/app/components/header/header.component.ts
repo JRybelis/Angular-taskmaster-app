ac3525f54d16d0f2486d2d442eaa8afcbc976f28
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-07-25 19:45:40 +0300

    Add toggle to the header button to switch between Add and Close

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  title = 'Angular Taskmaster';
+  title: string = 'Angular Taskmaster';
+  showAddTask!: boolean;
+  subscription: Subscription = new Subscription();
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
