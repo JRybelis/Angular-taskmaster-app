251fa68a54fb86b4889fc617cfdf45d533e8a681
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-12 18:16:47 +0300

    Add and register app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { FooterComponent } from './components/pages/footer/footer.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { FooterComponent } from './components/pages/footer/footer.component';
+import { UpdateTaskComponent } from './components/update-task/update-task.component';
 
 const appRoutes: Routes = [
   { path: '', component: TasksComponent },
@@ -24,17 +26,19 @@ const appRoutes: Routes = [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
+    AboutComponent,
+    FooterComponent,
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
-    AboutComponent,
-    FooterComponent,
+    UpdateTaskComponent,
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
+    AppRoutingModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
