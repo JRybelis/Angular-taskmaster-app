9af76e3243fa210535c2f5fb73d9c1f9bd1cb455
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-17 00:05:21 +0300

    Add toggleUpdateTask method to UiService

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { ITask } from '../ITask';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UiService {
-  private showAddTask: boolean = false;
+  private showAddTaskForm: boolean = false;
+  private showUpdateTaskForm: boolean = false;
   private subject = new Subject<any>();
 
   constructor() {}
 
   toggleAddTask(): void {
-    this.showAddTask = !this.showAddTask;
-    this.subject.next(this.showAddTask);
+    this.showAddTaskForm = !this.showAddTaskForm;
+    this.subject.next(this.showAddTaskForm);
+  }
+
+  toggleUpdateTask(task: ITask) {
+    // ar būtina paduoti task kai togglini šitą? ar reik task paduot update-task formoje tik?
+    this.showUpdateTaskForm = !this.showUpdateTaskForm;
+    this.subject.next(this.showUpdateTaskForm);
   }
 
   onToggle(): Observable<any> {
