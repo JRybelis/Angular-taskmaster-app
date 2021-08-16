17d6e76ad63dafebda1cbf1d67faa53f46a73dff
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-17 00:01:21 +0300

    Add update-task component.

---
+++ src/app/components/update-task/update-task.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { ITask } from 'src/app/ITask';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
+
+@Component({
+  selector: 'app-update-task',
+  templateUrl: './update-task.component.html',
+  styleUrls: ['./update-task.component.css'],
+})
+export class UpdateTaskComponent implements OnInit {
+  @Input() task!: ITask;
+  @Output() onUpdateTask: EventEmitter<ITask> = new EventEmitter();
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+  showUpdateTaskForm: boolean = false;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showUpdateTaskForm = value));
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task to update!');
+      return;
+    }
+    const updatedTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+      //ima iš ngmodel html'e
+    };
+
+    this.onUpdateTask.emit(updatedTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false; //what to do with this when it replaces the placeholder immediately?
+  }
+}
