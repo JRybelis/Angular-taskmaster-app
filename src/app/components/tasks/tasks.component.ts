d8aacebf999142520f24139377c71fd6006b90cb
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-17 17:48:43 +0300

    Add delete and update methods

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { TaskService } from 'src/app/services/task.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ITask } from 'src/app/ITask';
+import { UiService } from 'src/app/services/ui.service';
+import { TaskService } from 'src/app/services/task.service';
+import { faTimes, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +12,48 @@ import { ITask } from 'src/app/ITask';
 })
 export class TasksComponent implements OnInit {
   tasks: ITask[] = [];
+  @Input() task!: ITask;
+  @Input() updatedTask?: ITask;
+  faPencilAlt = faPencilAlt;
+  faTimes = faTimes;
 
-  constructor(private taskService: TaskService) {}
+  subscription?: Subscription; // ar nereikia = new Subscription()?
+  showUpdateTaskForm?: boolean = false;
+
+  @Output() onDeleteTask: EventEmitter<ITask> = new EventEmitter();
+  // @Output() onToggleReminder: EventEmitter<ITask> = new EventEmitter();
+
+  constructor(private taskService: TaskService, private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showUpdateTaskForm = value));
+  }
 
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  onAdd(task: ITask) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
+
+  onUpdate(updatedTask: ITask) {
+    this.taskService
+      .updateTask(updatedTask)
+      .subscribe(() => this.tasks.push(updatedTask));
+  }
+
+  toggleUpdateTask(task: ITask) {
+    this.uiService.toggleUpdateTask(task);
+  }
+
+  // onDelete(task: ITask) {
+  //   this.onDeleteTask.emit(task); //uztenka emitint ar reikia ir trint (deleteTask) sitam komponente?
+  // }
+
+  updateTask(task: ITask) {
+    // onUpdate gal vadinas dabar? sutikrink su html
+    // this.taskService.updateTask(task).subscribe((task) => this.tasks.push
   }
 
   deleteTask(task: ITask) {
@@ -27,9 +67,5 @@ export class TasksComponent implements OnInit {
   toggleReminder(task: ITask) {
     task.reminder = !task.reminder;
     this.taskService.updateTaskReminder(task).subscribe();
-  }
-
-  addTask(task: ITask) {
-    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
   }
 }
